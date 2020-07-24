8007a10df40b49053a357e2d8393e71986b415e8
Mathieu Poumeyrol <kali@zoy.org>
2020-07-24 13:37:51 +0200

    ConstantLike never made it to release

--- onnx/src/ops/array/mod.rs
+++ onnx/src/ops/array/mod.rs
@@ -7,12 +7,10 @@ use tract_hir::ops::array;
 
 use crate::model::{OnnxOpRegister, ParsingContext};
 use crate::pb::*;
-use tract_num_traits::AsPrimitive;
 
 pub fn register_all_ops(reg: &mut OnnxOpRegister) {
     reg.insert("Compress", compress::compress);
     reg.insert("Concat", concat);
-    reg.insert("ConstantLike", constant_like);
     reg.insert("ConstantOfShape", constant_of_shape);
     reg.insert("Expand", |_, _| Ok((expand(array::MultiBroadcastTo::default()), vec![])));
     reg.insert("EyeLike", eye_like);
@@ -22,11 +20,11 @@ pub fn register_all_ops(reg: &mut OnnxOpRegister) {
     reg.insert("Reshape", |_, _| Ok((expand(array::Reshape::default()), vec![])));
     reg.insert("Shape", |_, _| Ok((expand(array::Shape::new(DatumType::I64)), vec![])));
     reg.insert("Size", |_, _| Ok((expand(array::Size::new(DatumType::I64)), vec![])));
-    reg.insert("Transpose", transpose);
-    reg.insert("Tile", |_, _| Ok((expand(array::Tile::default()), vec![])));
     reg.insert("Slice", slice::slice);
     reg.insert("Split", split);
     reg.insert("Squeeze", squeeze);
+    reg.insert("Tile", |_, _| Ok((expand(array::Tile::default()), vec![])));
+    reg.insert("Transpose", transpose);
     reg.insert("Unsqueeze", unsqueeze);
 }
 
@@ -38,38 +36,12 @@ pub fn concat(
     Ok((expand(array::Concat::new(axis)), vec![]))
 }
 
-pub fn make_const<T>(shape: &[usize], v: f32) -> TractResult<Arc<Tensor>>
-where
-    T: Copy + Datum,
-    f32: AsPrimitive<T>,
-{
-    Ok(tract_ndarray::Array::<T, _>::from_elem(shape, v.as_()).into_arc_tensor())
-}
-
-pub fn constant_like(
-    _ctx: &ParsingContext,
-    node: &NodeProto,
-) -> TractResult<(Box<dyn InferenceOp>, Vec<String>)> {
-    let value = node.get_attr_opt("value")?.unwrap_or(0.);
-    if node.input.len() == 0 {
-        let dt = node.get_attr_opt("dtype")?.unwrap_or(f32::datum_type());
-        let shape: Vec<usize> = node.get_attr_vec("shape")?;
-        let tensor = dispatch_numbers!(self::make_const(dt)(&shape, value))?;
-        Ok((Box::new(tract_hir::ops::konst::Const::new(tensor)), vec![]))
-    } else {
-        Ok((Box::new(array::ConstantLike::new(value)), vec![]))
-    }
-}
-
 pub fn constant_of_shape(
     _ctx: &ParsingContext,
     node: &NodeProto,
 ) -> TractResult<(Box<dyn InferenceOp>, Vec<String>)> {
-    let value = match node.get_attr_opt::<Tensor>("value")? {
-        Some(val) => val.into_arc_tensor(),
-        None => make_const::<f32>(&vec![1], 0.0 as f32)?,
-    };
-    Ok((expand(array::ConstantOfShape::new(value)), vec![]))
+    let value = node.get_attr_opt::<Tensor>("value")?.unwrap_or(tensor0(0.0f32));
+    Ok((expand(array::ConstantOfShape::new(value.into_arc_tensor())), vec![]))
 }
 
 pub fn eye_like(
